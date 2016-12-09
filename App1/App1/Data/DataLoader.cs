be4d02f742a99243664bff17f5463951658159b8
stf <stefan.nilsson1@ikea.com>
2016-12-09 14:20:53 +0100

    Load json from portable in uwp

---
+++ App1/App1/Data/DataLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace App1.Data
+{
+    public static class DataLoader
+    {
+        public static async Task<string> LoadJsonAsync()
+        {
+            string fileName = "ms-appx:///JsonData/data.json";
+            try
+            {
+                var fileUri = new Uri(fileName);
+                var storageFile = await StorageFile.GetFileFromApplicationUriAsync(fileUri).AsTask().ConfigureAwait(false);
+                var json = await FileIO.ReadTextAsync(storageFile).AsTask().ConfigureAwait(false);
+
+                return json;
+
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception.Message);
+                return string.Empty;
+            } 
+        }
+    }
+}
---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using App1.Data;
+
+
+namespace App1
+{
+
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var data = await LoadAsync();
+            if (data == null)
+            {
+                return;
+            }
+
+            dataTextBlock.Text = data;
+        }
+
+        private async Task<string> LoadAsync()
+        {
+            var data = await DataLoader.LoadJsonAsync().ConfigureAwait(false);
+            return data;
+        }
+    }
+}
